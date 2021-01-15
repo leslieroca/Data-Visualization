5e7e0a33c3fa7ade064f134a1a7082021f8ea75f
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2021-01-14 20:42:22 -0500

    Parsing the CSV File Headers
    Set of CSV-formatted weather data recorded in Sitka

---
+++ highs_lows.py
@@ -0,0 +1,1 @@
+import csvfilename = 'sitka_weather_07-2014.csv'with open(filename) as f:    reader = csv.reader(f)    header_row = next(reader)    for index, column_header in enumerate(header_row):        print(index, column_header)
