2e8919f0d1a40e7f298b48c7163fff2597222106
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2021-01-15 22:32:39 -0500

    Extracting and Reading Data

--- highs_lows.py
+++ highs_lows.py
@@ -1,1 +1,1 @@
-import csvfilename = 'sitka_weather_07-2014.csv'with open(filename) as f:    reader = csv.reader(f)    header_row = next(reader)    for index, column_header in enumerate(header_row):        print(index, column_header)
+import csv# Get high temperatures from file.filename = 'sitka_weather_07-2014.csv'with open(filename) as f:    reader = csv.reader(f)    header_row = next(reader)    highs = []    for row in reader:        high = int(row[1])        highs.append(high)    print(highs)
