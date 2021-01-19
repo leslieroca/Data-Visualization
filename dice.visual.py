038b32162bb50a92f9e6f2381e504a33c8ea612b
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2021-01-18 22:11:30 -0500

    Switching line separator from "Classic macOS (\r)" to "Unix and macOS (\n)"

--- dice.visual.py
+++ dice.visual.py
@@ -1,1 +1,32 @@
-import pygalfrom die import Die# Create two D6 dice.die_1 = Die()die_2 = Die()#  Make some rolls, and store the results in a list.results = []for roll_num in range(1000):    result = die_1.roll() + die_2.roll()    results.append(result)# Analyze the results.frequencies = []max_result = die_1.num_sides + die_2.num_sidesfor value in range(2, max_result+1):    frequency = results.count(value)    frequencies.append(frequency)# Visualize th resultshist = pygal.Bar()hist.title = "Results of rolling two D6 1000 times."hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']hist.x_title = "Possible results of rolling two D6"hist.y_title = "Frequency of Result"hist.add('D6 + D6', frequencies)hist.render_to_file('dice.visual.svg')
+import pygal
+
+from die import Die
+
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die()
+
+#  Make some rolls, and store the results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize th results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Possible results of rolling two D6"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('dice.visual.svg')
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,1 +1,21 @@
-import matplotlib.pyplot as pltx_values = list(range(1, 1001))y_values = [x**2 for x in x_values]plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)# Set chart title and label axes.plt.title("Square Numbers", fontsize=24)plt.xlabel("Value", fontsize=14)plt.ylabel("Square of Value", fontsize=14)# Set size of tick labels.plt.tick_params(axis='both', which='major', labelsize=14)# Set the range for each axis.plt.axis([0, 1100, 0, 1100000])# Automatically save the plot to a file.# The second argument trims extra whitespace from the plot. If you want the extra whitespace around the plot, you can omit this argument.plt.savefig('squares_plot.png', bbox_inches='tight')# plt.show()
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
+
+# Automatically save the plot to a file.
+# The second argument trims extra whitespace from the plot. If you want the extra whitespace around the plot, you can omit this argument.
+plt.savefig('squares_plot.png', bbox_inches='tight')
