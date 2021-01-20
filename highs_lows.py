99a75b2b309ae18d6ed391fb23218fe0dc36f616
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2021-01-19 19:15:59 -0500

    Shading an Area in the Chart

--- highs_lows.py
+++ highs_lows.py
@@ -24,8 +24,9 @@ with open(filename) as f:
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c= 'blue')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c= 'blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 
 # Format plot.
