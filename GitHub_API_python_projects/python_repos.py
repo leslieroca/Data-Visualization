ad6b11c858882dfd8615442be50f739e901d80dc
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-01-30 21:26:56 -0500

    Working with the Response Dictionary

--- GitHub_API_python_projects/python_repos.py
+++ GitHub_API_python_projects/python_repos.py
@@ -8,6 +8,16 @@ print(f"Status code: {r.status_code}")
 
 # Store API response in a variable.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
